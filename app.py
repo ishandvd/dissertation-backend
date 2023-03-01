8338492c1e555e9b6807c231b4044b011537cb33
ishandvd <id360@cam.ac.uk>
2023-03-01 23:24:07 +0000

    Im giving up on trying to use sockets, im just going to use HTTP requests and make it stateless on the server side

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 from datetime import datetime
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import os
 import time
 from werkzeug.utils import secure_filename
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS
+
 
 UPLOAD_FOLDER = './audio_uploads'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  # 100MB
+CORS(app,resources={r"/*":{"origins":"*"}})
+socketio = SocketIO(app,cors_allowed_origins="*",message_queue_max_size=50000000, message_queue_timeout=60)
 
 ALLOWED_EXTENSIONS = {'wav'}
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -20,6 +38,36 @@ def index():
    print('Request for index page received')
    return {"status": "ok", "message": "Hello World!"}
 
+
+@socketio.on("connect")
+def connected():
+    """event listener when client connects to the server"""
+    print(request.sid)
+    print("client has connected")
+    emit("connect",{"data":f"id: {request.sid} is connected"})
+
+@socketio.on("disconnect")
+def disconnected():
+    """event listener when client disconnects to the server"""
+    print("user disconnected")
+    emit("disconnect",f"user {request.sid} disconnected",broadcast=True)
+
+@socketio.on("backingtrack")
+def backing_times(audio):
+    print(f"{bcolors.OKGREEN}attempting to store backing track{bcolors.ENDC}")
+    filename = time.strftime("%Y%m%d-%H%M%S") + ".wav"
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    with open(file_path, 'wb') as f:
+        f.write(audio)
+    # chunk_size = 1024  # set chunk size to 1 KB
+    # with open(file_path, 'ab') as f:
+    #     while True:
+    #         chunk = audio.read(chunk_size)
+    #         if not chunk:
+    #             break
+    #         f.write(chunk)
+    print("backing track stored")
+    emit("backingtrack",f"You just submitted a file properly (hopefully)")
 
 @app.route('/audio-upload', methods=['POST'])
 def audio_upload():
@@ -34,4 +82,4 @@ def audio_upload():
         return {"status": "ok", "message": "Hello World!"}, 200
 
 if __name__ == '__main__':
-   app.run(debug=True)
+   socketio.run(app, debug=True)
